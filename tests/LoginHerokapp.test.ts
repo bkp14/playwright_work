86e294da877450828d422bf0711d7fadc53b6363
krishnaprasath <prasathkrishna002@gmail.com>
2026-06-24 16:50:47 +0530

    implmented playwright basics

---
+++ tests/LoginHerokapp.test.ts
@@ -0,0 +1,26 @@
+import {test,expect} from '@playwright/test';
+test.beforeEach(async({page})=>{
+    await page.goto(process.env.base_url!)
+} )
+test.describe('Login Module',()=>{
+test('valid Login Test',async ({page}) => {
+    await page.fill('#username',process.env.uname!);
+    await page.fill('#password',process.env.pword!);
+    await page.click('.fa.fa-2x.fa-sign-in');
+    await page.isVisible('.icon-2x.icon-signout')
+    await expect(page.locator('.flash.success')).toBeVisible();
+});
+
+test ('invalid login test',async({page}) => {
+    await page.fill('#username',"dsb");
+    await page.fill('#password',"dsaw2");
+    await page.click('.fa.fa-2x.fa-sign-in'); 
+    await expect(page.locator("//div[@id='flash']"))
+    .toContainText('Your username is invalid!');
+
+
+})
+})
+test.afterEach(async({page})=>{
+  console.log("test completed!")
+})
